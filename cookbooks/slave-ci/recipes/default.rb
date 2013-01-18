dc5767936812e142881a8bf58227060cb3a32896
jpd4nt <jdrawneek@nationaltheatre.org.uk>
2013-01-18 23:04:08 +0000

    selenium start script

--- cookbooks/slave-ci/recipes/default.rb
+++ cookbooks/slave-ci/recipes/default.rb
@@ -6,7 +6,6 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
 cookbook_file "/etc/hosts" do
     source "hosts"
     owner  "root"
@@ -17,9 +16,11 @@ end
 case node['platform_family']
 when "ubuntu"
   vhost_path = '/etc/apache2/sites-available/vhost'
-when "centos"
+when "rhel", "fedora", "centos"
   vhost_path = '/etc/httpd/conf.d/vhost.conf'
 when "amazon"
+  vhost_path = '/etc/httpd/conf.d/vhost.conf'
+else
   vhost_path = '/etc/httpd/conf.d/vhost.conf'
 end
 
@@ -53,17 +54,17 @@ unless FileTest.exists?("/tmp/#{node['slave-ci']['selenium_file']}")
     path "/tmp/#{node['slave-ci']['selenium_file']}"
     source "http://selenium.googlecode.com/files/#{node['slave-ci']['selenium_file']}"
   end
-  bash "run-selenium" do
-    code "(cd /tmp; java -jar #{node['slave-ci']['selenium_file']})"
-  end
 end
 
-remote_file "slave" do
-  path "/tmp/slave.jar"
-  source "#{node['slave-ci']['jenkins']['host']}:#{node['slave-ci']['jenkins']['port']}/#{node['slave-ci']['jenkins']['prefix']}/#{node['slave-ci']['jenkins']['slave_uri']}"
+template '/etc/init.d/selenium' do
+  source "selenium.erb"
+  owner  "root"
+  group  "root"
+  mode   "0755"
+  variables()
 end
 
-execute "slave" do
-  command "java -jar /tmp/slave.jar -jnlpUrl #{node['slave-ci']['jenkins']['host']}:#{node['slave-ci']['jenkins']['port']}/#{node['slave-ci']['jenkins']['prefix']}/computer/#{node['slave-ci']['nodename']}/slave-agent.jnlp"
-  user node['slave-ci']['user']
+service "selenium" do
+  supports :status => true, :restart => true
+  action [ :enable, :start ]
 end
