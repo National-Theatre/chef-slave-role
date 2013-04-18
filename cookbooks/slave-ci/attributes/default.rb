642bdeb342fbeb2730fd3a86be18397c664c0b7b
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2013-04-18 16:24:02 +0100

    update settings

--- cookbooks/slave-ci/attributes/default.rb
+++ cookbooks/slave-ci/attributes/default.rb
@@ -7,11 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
+default['slave-ci']['jenkins']['workspace'] = "/home/ec2-user/workspace" 
 default['slave-ci']['jenkins']['host']      = "http://localhost"
 default['slave-ci']['jenkins']['port']      = "8080"
 default['slave-ci']['jenkins']['prefix']    = "" 
 default['slave-ci']['jenkins']['slave_uri'] = "/jnlpJars/slave.jar"
 default['slave-ci']['user']                 = "apache"
 default['slave-ci']['nodename']             = "test"
-default['slave-ci']['chrome_file']          = "chromedriver_linux32_23.0.1240.0.zip"
-default['slave-ci']['selenium_file']        = "selenium-server-standalone-2.28.0.jar"
+default['slave-ci']['chrome_file']          = "chromedriver2_linux64_0.7.zip"
+default['slave-ci']['selenium_file']        = "selenium-server-standalone-2.32.0.jar"
--- cookbooks/slave-ci/recipes/default.rb
+++ cookbooks/slave-ci/recipes/default.rb
@@ -25,29 +25,29 @@ else
   vhost_path = '/etc/httpd/conf.d/vhost.conf'
 end
 
-if FileTest.exists?("/var/jenkins/workspace/bpa")
+if FileTest.exists?("#{node['slave-ci']['jenkins']['workspace']}/bpa")
   web_app "bpa" do
     server_name 'www.bpa.test.local'
     server_aliases []
-    docroot "/var/jenkins/workspace/bpa/drupal"
+    docroot "#{node['slave-ci']['jenkins']['workspace']}/bpa/drupal"
     allow_override "All"
   end
 end
 
-if FileTest.exists?("/var/jenkins/workspace/website")
+if FileTest.exists?("#{node['slave-ci']['jenkins']['workspace']}/website")
   web_app "nt_website" do
     server_name 'cms.nationaltheatre.test.local'
     server_aliases []
-    docroot "/var/jenkins/workspace/website/drupal"
+    docroot "#{node['slave-ci']['jenkins']['workspace']}/website/drupal"
     allow_override "All"
   end
 end
 
-if FileTest.exists?("/var/jenkins/workspace/pp")
+if FileTest.exists?("#{node['slave-ci']['jenkins']['workspace']}/pp")
   web_app "nt_pp" do
     server_name 'secure.nationaltheatre.test.local'
     server_aliases []
-    docroot "/var/jenkins/workspace/pp/drupal/pp/web"
+    docroot "#{node['slave-ci']['jenkins']['workspace']}/pp/drupal/pp/web"
     allow_override "All"
   end
 end
