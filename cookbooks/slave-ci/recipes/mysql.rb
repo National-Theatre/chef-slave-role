60c2d889ab8cc910a27ea42edaf48be34abf8763
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2014-06-06 15:50:00 +0100

    fix mysql restart

--- cookbooks/slave-ci/recipes/mysql.rb
+++ cookbooks/slave-ci/recipes/mysql.rb
@@ -24,6 +24,25 @@ execute "Create_tmpfs" do
   action :run
 end
 
+cookbook_file "/etc/init.d/init_mysql" do
+    source "hosts"
+    owner  "root"
+    group  "root"
+    mode   "0755"
+end
+
+link "/etc/rc3.d/S11init_mysql" do
+  action :create
+  to "/etc/init.d/init_mysql"
+  not_if {File.exists?("/etc/rc3.d/S11init_mysql")}
+end
+
+link "/etc/rc5.d/S11init_mysql" do
+  action :create
+  to "/etc/init.d/init_mysql"
+  not_if {File.exists?("/etc/rc5.d/S11init_mysql")}
+end
+
 include_recipe "build-essential::default"
 include_recipe "mysql::server"
 include_recipe "mysql::client"
